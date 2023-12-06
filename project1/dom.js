2bb89c754aea5c418c8f80599d70a2f12a85de79
mdruhaib23 <mohammedruhaib89@gmail.com>
2023-12-06 23:25:08 +0530

    changes

--- project1/dom.js
+++ project1/dom.js
@@ -1,4 +1,4 @@
-//examine the document object
+// //examine the document object
 console.dir(document);
 console.log(document.dom);
 console.log(document.URL);
@@ -23,3 +23,49 @@ console.log(headertitle.innerText);
 headertitle.innerHTML = '<h3>Hello</h3>';
 header.style.borderBottom = 'solid 3px #000';
 
+// GETELEMENTSBYCLASSNAME //
+var li = document.getElementsByTagName('list-group-item');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor='green';
+
+// items.style.backgroundColor = '#f4f4f4';
+
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor = '#f4f4f4';
+}
+
+// QUERYSELECTOR //
+var header = document.querySelector('#main-header');
+header.style.borderBottom ='solid 4px #ccc';
+
+var input = document.querySelector('input')
+input.value = 'hello World'
+
+var submit = document.querySelector('input[type="submit"]');
+submit,it.value="SEND"
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastitem = document.querySelector('.list-group-item:last-chiild');
+lastitem.style.color = ' blue';
+
+var seconditem = document.querySelector('.list-group-item:nthchiild(2)');
+seconditem.style.color = 'coral';
+
+
+//QUERYSELECTORALL //
+var title = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i = 0; i < odd,length; i++){
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
+}
