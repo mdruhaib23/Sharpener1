8a2b01dc64373659c0249b5ff449d46a8672ca92
mdruhaib23 <mohammedruhaib89@gmail.com>
2023-12-08 15:38:44 +0530

    changes

--- Project2/main.js
+++ Project2/main.js
@@ -1,11 +1,13 @@
 var form = document.getElementById('addForm');
 var itemlist = document.getElementById('items');
+var filter = document.getElementById('filter')
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delet event
 itemlist.addEventListener('click', remeveItem);
-
+// Filter event
+filter.addEventListener('keyup', filterItems)
 
 //Add item
 function addItem(e){
@@ -45,4 +47,21 @@ function removeItem(e){
             itemlist.removeChild(li);
         }
     }
+}
+
+// filter items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.tolowerCase();
+    // Get li's
+    itemlist.getElementsByTagName('li');
+    // convert to an array
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.tolowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
 }
